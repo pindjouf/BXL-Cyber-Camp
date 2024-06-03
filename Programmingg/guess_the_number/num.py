94d1c8f4ab3b9dd22ef4a8b6760e2579642aa74d
pindjouf <esaubukasa@protonmail.com>
2024-06-04 00:55:32 +0200

    code cleanup

--- Programmingg/guess_the_number/num.py
+++ Programmingg/guess_the_number/num.py
@@ -1,3 +1,18 @@
 import random
+import os
 
+solution = random.randrange(0, 100)
+answer = int(input("Enter the number: "))
+tries = 5
 
+print(solution)
+
+while answer != solution and tries > 0:
+    print(tries, "tries left")
+    tries -= 1
+    answer = int(input("Enter the number: "))
+    os.system('clear')
+    if (answer == solution):
+        print("GG bro :D")
+    else:
+        print("dumbass")
