ba3ab4c5af9e69009aeda108a45c813dd5a724ed
pindjouf <esaubukasa@protonmail.com>
2024-06-07 14:30:22 +0200

    new file:   Programmingg/pinger/.gitignore 	new file:   Programmingg/pinger/demofile.txt 	modified:   Programmingg/pinger/pinger.py 	new file:   Programmingg/pinger/test.py

---
+++ Programmingg/pinger/test.py
@@ -0,0 +1,14 @@
+li = []
+names = ['bob', 'alice', 'john']
+length = len(li)
+
+print("This list has {} items". format(length))
+
+for name in names:
+    li.append(names)
+    string = f"{name}"
+    print(string)
+
+lengthh = len(li)
+print("This list now has {} items". format(lengthh))
+
