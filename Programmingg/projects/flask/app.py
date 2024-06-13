daa066b04620b59a637e81fe058741e75b1cb262
pindjouf <esaubukasa@protonmail.com>
2024-06-13 15:11:12 +0200

    modified:   Programmingg/projects/flask/app.py 	modified:   Programmingg/projects/flask/templates/contact.html

--- Programmingg/projects/flask/app.py
+++ Programmingg/projects/flask/app.py
@@ -4,12 +4,14 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     return "<h1>Hackers Poullete</h1><br><a href='/contact'>Contact</a>"
 
 @app.route("/contact")
 def contact():
     return render_template('contact.html')
 
-
+@app.route("/submit")
+def submit():
+    return "<h1>Hackers Poullete</h1><br><a href='/contact'>Contact</a>"
 
