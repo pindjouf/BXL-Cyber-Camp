9e8a1b3ec1a7f4e3296b00192c83a9463b118f87
pindjouf <esaubukasa@protonmail.com>
2024-06-16 00:35:23 +0200

    db init

--- Programmingg/projects/flask/app.py
+++ Programmingg/projects/flask/app.py
@@ -1,7 +1,18 @@
+from datetime import datetime
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, g
+import sqlite3
+from markupsafe import escape
 
 app = Flask(__name__)
+
+DATABASE = 'databse'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
 @app.route("/")
 def home():
@@ -11,19 +22,22 @@ def home():
 def contact():
     return render_template('contact.html')
 
-@app.route("/submit")
+@app.route("/submit", methods=['POST'])
 def submit():
-    first_name = request.form.get("first_name")
-    last_name = request.form.get("last_name")
-    email = request.form.get("email")
-    country = request.form.get("country")
-    message = request.form.get("message")
-    gender = request.form.get("gender")
-    subject = request.form.get("subject")
-
     if request.method == 'POST':
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        email = request.form.get("email")
+        country = request.form.get("country")
+        message = request.form.get("message")
+        gender = request.form.get("gender")
+        subject = request.form.get("subject")
+        action = request.form.get("submit")
+        # now = str(datetime.now())
+        if action == 'submit':
+            get_db()
 
-    return "<h1>Hackers Poullete</h1><br><a href='/contact'>Contact</a>"
+    return f"Hello, {escape(first_name)}!"
 
 if __name__ == "__main__":
     app.run()
