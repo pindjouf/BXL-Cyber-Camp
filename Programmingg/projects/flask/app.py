e45e5499d119ca3e2a8212406757c0c3534d233f
pindjouf <esaubukasa@protonmail.com>
2024-06-15 23:41:47 +0200

    POST

--- Programmingg/projects/flask/app.py
+++ Programmingg/projects/flask/app.py
@@ -13,5 +13,17 @@ def contact():
 
 @app.route("/submit")
 def submit():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    country = request.form.get("country")
+    message = request.form.get("message")
+    gender = request.form.get("gender")
+    subject = request.form.get("subject")
+
+    if request.method == 'POST':
+
     return "<h1>Hackers Poullete</h1><br><a href='/contact'>Contact</a>"
 
+if __name__ == "__main__":
+    app.run()
