56668e4613d21572d1012b6d906cb7dee40bfb19
pindjouf <esaubukasa@protonmail.com>
2024-06-18 17:12:53 +0200

    beautiful project

--- Programmingg/projects/flask/app.py
+++ Programmingg/projects/flask/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import sqlite3
-from datetime import timedelta, datetime
+from datetime import datetime
 from dotenv import load_dotenv
 import os
 import html
